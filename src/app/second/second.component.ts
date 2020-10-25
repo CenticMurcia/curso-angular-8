095c1406fe8be4cdb16c31c736e09d34a8eac825
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-10-25 11:26:40 +0100

    create directives ngFor and nfIf

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
+  // Atributos del Componente
+  public arrayNames: Array<string>;
+  public showMessage: boolean;
 
-  constructor() { }
+  constructor() {
+    this.arrayNames = ['Marcos', 'Pedro', 'Carmen', 'Jonatan', 'Franciso'];
+    this.showMessage = false;
+  }
 
   ngOnInit() {
+  }
+
+  public toggleMessage(): void {
+
+    if (this.showMessage) {
+      this.showMessage = false;
+    } else {
+      this.showMessage = true;
+    }
+
+    // this.showMessage ? this.showMessage = false : this.showMessage = true;
   }
 
 }
