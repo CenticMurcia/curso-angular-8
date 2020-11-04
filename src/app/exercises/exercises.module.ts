dc7a94f610e860685f69822f7218ef43929a80d5
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-11-04 19:37:10 +0100

    Exercises Relacion-componentes

--- src/app/exercises/exercises.module.ts
+++ src/app/exercises/exercises.module.ts
@@ -9,9 +9,23 @@ import { InfoGithubComponent } from './arquitectura-angular/components/info-gith
 import { SubAppComponent } from './sub-app.component';
 import { ComponentOneComponent } from './navegacion-rutas/components/component-one/component-one.component';
 import { ComponentTwoComponent } from './navegacion-rutas/components/component-two/component-two.component';
+import { CategoriasComponent } from './relacion-componentes/components/categorias/categorias.component';
+import { PalabraComponent } from './relacion-componentes/components/categorias/palabra/palabra.component';
+
+import { CategoriasService } from './relacion-componentes/services/categorias.service';
+import { ShareDataService } from './relacion-componentes/services/share-data.service';
+import { CategoriaComponent } from './relacion-componentes/components/categoria/categoria.component';
 
 @NgModule({
-  declarations: [SaludarComponent, InfoGithubComponent, SubAppComponent, ComponentOneComponent, ComponentTwoComponent],
+  declarations: [
+    SaludarComponent,
+    InfoGithubComponent,
+    SubAppComponent,
+    ComponentOneComponent,
+    ComponentTwoComponent,
+    CategoriasComponent,
+    PalabraComponent,
+    CategoriaComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -21,10 +35,13 @@ import { ComponentTwoComponent } from './navegacion-rutas/components/component-t
         { path: 'arquitectura', component: SaludarComponent},
         { path: 'component-one/:name', component: ComponentOneComponent},
         { path: 'component-two', component: ComponentTwoComponent},
+        { path: 'categorias', component: CategoriasComponent},
+        { path: 'categoria', component: CategoriaComponent},
 
         { path: '', redirectTo: '/exercises/arquitectura', pathMatch: 'full' }
       ]},
     ])
-  ]
+  ],
+  providers: [CategoriasService, ShareDataService]
 })
 export class ExercisesModule { }
