cc0dfefa31ccfc075f4cda8f917d021a13a8146a
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-11-12 00:42:03 +0100

    exercise 'Angular avanzado'

---
+++ src/app/exercises/angular-avanzado/auth-guard.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuardGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if (!sessionStorage.getItem('appToken')) {
+        this.router.navigate(['/exercises/login'], {replaceUrl: true});
+        return false;
+      }
+
+      return true;
+  }
+
+}
