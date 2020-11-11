7f88f22e653edf80fdff8efd59bc3a6ff047cad9
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-11-11 23:05:02 +0100

    bug private/public atributes

--- src/app/exercises/relacion-componentes/components/categoria/categoria.component.ts
+++ src/app/exercises/relacion-componentes/components/categoria/categoria.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaComponent implements OnInit {
 
-  private categoria: Categoria;
+  public categoria: Categoria;
 
   constructor(private shareDataService: ShareDataService) { }
 
