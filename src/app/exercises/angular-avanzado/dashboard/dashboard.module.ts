699a319b5555eb22f2f5b2d0296fb961b9c31fca
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-11-12 10:49:06 +0100

    Exercises 'Angular Material'

--- src/app/exercises/angular-avanzado/dashboard/dashboard.module.ts
+++ src/app/exercises/angular-avanzado/dashboard/dashboard.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
 import { SaludarComponent } from '../../arquitectura-angular/components/saludar/saludar.component';
 import { InfoGithubComponent } from '../../arquitectura-angular/components/info-github/info-github.component';
 import { ComponentOneComponent } from '../../navegacion-rutas/components/component-one/component-one.component';
@@ -28,7 +35,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
         { path: '', redirectTo: 'arquitectura', pathMatch: 'full' }
       ]},
     ]),
-    FormsModule
+    FormsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ]
 })
 export class DashboardModule { }
--- src/app/exercises/angular-avanzado/login/login.module.ts
+++ src/app/exercises/angular-avanzado/login/login.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -12,7 +17,11 @@ import { LoginComponent } from './login/login.component';
     RouterModule.forChild([
       { path: '', component: LoginComponent}
     ]),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
